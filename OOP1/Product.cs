32d39055e9178e5975ef4b32301f826717fbeae6
operator <operator@DESKTOP-MC9O68R>
2021-01-20 21:40:39 +0300

    Week 4

--- OOP1/Product.cs
+++ OOP1/Product.cs
@@ -7,7 +7,10 @@ namespace OOP1
     class Product
     {
         public int Id { get; set; }
+        public int CategoryId { get; set; }
         public string ProductName { get; set; }
         public double UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
+
     }
 }
